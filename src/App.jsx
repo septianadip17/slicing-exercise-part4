ffd78470d20a0b0810ba27dc7a2fcc9cb92972eb
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-21 12:45:44 +0700

    services added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import Services from "./components/Services";
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
+      <Services />
     </>
   );
 }
