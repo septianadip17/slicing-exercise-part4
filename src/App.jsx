5a54a6bc5cc04a914f5ef1104493b07db13adab3
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-21 11:07:55 +0700

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline text-brandPrimary">Hello world!</h1>
+      <Navbar/>
     </>
   );
 }
