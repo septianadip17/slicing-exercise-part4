e9c512108c97d42728b0e3152fe7a95f43e75c42
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-21 11:04:18 +0700

    color setting and font installed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'neutralSilver': "#F5F7FA",
+        'neutralDGrey': "#4D4D4D",
+        'brandPrimary': "#4CAF4F",
+        'neutralGrey': "#717171",
+      }
+    },
   },
   plugins: [],
 };
