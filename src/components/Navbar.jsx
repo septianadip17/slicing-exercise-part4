d8908ae3ae25cc27b8285a4edabccde1798b9cee
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-21 12:01:52 +0700

    navbar finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import logo from "../assets/logo.png";
 import { Link } from "react-scroll";
 
+// react icons
+import { FaXmark, FaBars } from "react-icons/fa6";
+
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isSticky, setIsSticky] = useState(false);
@@ -37,8 +40,14 @@ const Navbar = () => {
 
   return (
     <header className="w-full bg-white md:bg-transparent fixed top-0 left-0 right-0">
-      <nav>
-        <div>
+      <nav
+        className={`py-4 lg:px-14 ${
+          isSticky
+            ? "sticky top-0 left-0 right-0 border-b bg-white duration-300"
+            : ""
+        }`}
+      >
+        <div className="flex justify-between items-center text-base gap-8">
           <a
             href=""
             className="text-2xl font-semibold flex items-center space-x-3"
@@ -77,6 +86,38 @@ const Navbar = () => {
           </div>
 
           {/* menu button for only mobile devices */}
+          <div className="md:hidden">
+            <button
+              onClick={toggleMenu}
+              className="text-neutralDGrey focus:outline-none focus:text-gray-500"
+            >
+              {isMenuOpen ? (
+                <FaXmark className="h-6 w-6" />
+              ) : (
+                <FaBars className="h-6 w-6" />
+              )}
+            </button>
+          </div>
+        </div>
+
+        {/* nav items for mobile devices */}
+        <div
+          className={`space-y-4 px-4 mt-16 py-7 bg-brandPrimary ${
+            isMenuOpen ? "block fixed top-0 right-0 left-0" : "hidden"
+          }`}
+        >
+          {navItems.map(({ link, path }) => (
+            <Link
+              to={path}
+              spy={true}
+              smooth={true}
+              offset={-100}
+              key={path}
+              className="block text-base text-white hover:text-brandPrimary first:font-medium"
+            >
+              {link}
+            </Link>
+          ))}
         </div>
       </nav>
     </header>
