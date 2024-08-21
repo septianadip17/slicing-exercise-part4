d7461258b95dc11703e04a2a4301536a42a3b02e
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-21 13:02:17 +0700

    company logo

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <div className='md:px-14 px-4 py-16 max-w-screen-2xl mx-auto'>
+      <div className="text-center my-8">
+        <h2 className="text-4xl text-neutralDGrey font-semibold mb-2">Our Clients</h2>
+        <p className="text-neutralDGrey">We have been working with some Fortune 500+ clients</p>
+
+        {/* company logo */}
+        <div className=''>
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
   )
 }
 
