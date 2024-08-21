371ec7a0c9ac12c31b3bf9c23919c7398a5696a7
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-21 11:32:47 +0700

    color added variant

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,12 @@ export default {
   theme: {
     extend: {
       colors: {
-        'neutralSilver': "#F5F7FA",
-        'neutralDGrey': "#4D4D4D",
-        'brandPrimary': "#4CAF4F",
-        'neutralGrey': "#717171",
-      }
+        neutralSilver: "#F5F7FA",
+        neutralDGrey: "#4D4D4D",
+        brandPrimary: "#4CAF4F",
+        neutralGrey: "#717171",
+        gray900: "#18191F",
+      },
     },
   },
   plugins: [],
