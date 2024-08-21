a2a342b941c8a4598c3f91f8143a550bd85d8fcc
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-21 13:07:20 +0700

    company logo showed

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const Services = () => {
   return (
-    <div className='md:px-14 px-4 py-16 max-w-screen-2xl mx-auto'>
+    <div className="md:px-14 px-4 py-16 max-w-screen-2xl mx-auto">
       <div className="text-center my-8">
-        <h2 className="text-4xl text-neutralDGrey font-semibold mb-2">Our Clients</h2>
-        <p className="text-neutralDGrey">We have been working with some Fortune 500+ clients</p>
+        <h2 className="text-4xl text-neutralDGrey font-semibold mb-2">
+          Our Clients
+        </h2>
+        <p className="text-neutralDGrey">
+          We have been working with some Fortune 500+ clients
+        </p>
 
         {/* company logo */}
-        <div className=''>
-          <img src="" alt="" />
+        <div className="my-12 flex flex-wrap justify-between items-center gap-8">
+          <img src="/src/assets/company1.png" alt="company 1" />
+          <img src="/src/assets/company2.png" alt="company 2" />
+          <img src="/src/assets/company3.png" alt="company 3" />
+          <img src="/src/assets/company4.png" alt="company 4" />
+          <img src="/src/assets/company5.png" alt="company 5" />
+          <img src="/src/assets/company6.png" alt="company 6" />
+          <img src="/src/assets/company7.png" alt="company 7" />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
