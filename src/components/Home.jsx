776b3b2978afd854b85255caddaab725d217acf7
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-21 12:36:49 +0700

    btn primary

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,32 @@
 import { Carousel } from "flowbite-react";
 import React from "react";
+import banner1 from "../assets/banner1.png";
 
 const Home = () => {
   return (
     <div className="bg-neutralSilver">
       <div className="px-4 lg:px-14 max-w-screen-2xl mx-auto min-h-screeen h-screen">
-        <Carousel>
-          <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-            Slide 1
-          </div>
-          <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-            Slide 2
-          </div>
-          <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-            Slide 3
+        <Carousel className="w-full mx-auto">
+          <div className="my-28 md:my-8 py-12 flex flex-col md:flex-row-reverse items-center justify-between gap-12">
+            <div>
+              <img src={banner1} alt="banner" />
+            </div>
+            {/* hero text */}
+            <div className="md:w-1/2">
+              <h1 className="text-5xl font-semibold mb-4 text-neutralDGrey md:3/4 leading-snug">
+                Lesson and insight{" "}
+                <span className="text-brandPrimary leading-snug">
+                  from 8 years
+                </span>
+              </h1>
+              <p className="text-neutralGrey text-base mb-8">
+                Where to grow your business as a photographer: site or social
+                media?
+              </p>
+              <button className="btn-primary">
+                Register
+              </button>
+            </div>
           </div>
         </Carousel>
       </div>
