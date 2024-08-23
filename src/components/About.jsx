5fa2adbbba7333daf155dd96e3e79b02987081a8
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-23 20:29:41 +0700

    txt to text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,13 +5,13 @@ const About = () => {
   return (
     <div>
       {/* about text */}
-      <div className="px-4 lg:px-14 max-w-screen-2xl mx-auto my-8">
-        <div className="md:w-11/12 mx-auto flex flex-col md:flex-row justify-between items-center gap-12">
+      <div className="px-4 lg:px-14 max-w-screen-2xl mx-auto my-8 border-2 border-red-600">
+        <div className="md:w-11/12 mx-auto flex flex-col md:flex-row justify-between items-center gap-12 border-2 border-blue-600">
           <div>
             <img src={aboutImg} alt="about image" />
           </div>
           <div className="md:w-3/5 mx-auto">
-            <h2 className="txt-4xl text-neutralDGrey font-semibold mb-4 md:w-4/5">
+            <h2 className="text-4xl text-neutralDGrey font-semibold mb-4 md:w-4/5">
               The unseen of spending three years at Pixelgrade
             </h2>
             <p className="md:w-3/4 text-sm text-neutralGrey mb-8">
