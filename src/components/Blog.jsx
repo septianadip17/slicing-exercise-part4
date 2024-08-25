4f7bfe07f17bdab10f07cb715a533a860e480cfa
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-25 21:02:00 +0700

    array blogs added

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
 
 const Blog = () => {
-  return (
-    <div>Blog</div>
-  )
-}
+  const blogs = [
+    {
+      id: 1,
+      title: "Creating Streamlined Safeguarding Processes with OneRen",
+      image: "/src/assets/blog1.png",
+    },
+    {
+      id: 2,
+      title:
+        "What are your safeguarding responsibilities and how can you manage them? ",
+      image: "/src/assets/blog2.png",
+    },
+    {
+      id: 3,
+      title: "Revamping the Membership Model with Triathlon Australia",
+      image: "/src/assets/blog3.png",
+    },
+  ];
 
-export default Blog
+  return (
+    <div className="px-4 lg:px-14 max-w-screen-2xl mx-auto my-12">
+      <div className="text-center md:w-1/2 mx-auto my-12">
+        <h2 className="text-4xl text-neutralDGrey font-semibold mb-4">
+          Caring is the new marketing
+        </h2>
+        <p className="text-sm text-neutralGrey mb-8 md:w-3/4 mx-auto">
+          The Nexcent blog is the best place to read about the latest membership
+          insights, trends and more. See who is joining the community, read
+          about how our community are increasing their membership income and
+          lots more.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
